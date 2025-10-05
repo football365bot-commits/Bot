fa63b47ff0a00dd998249066386954f6ea35cfd2
football365bot-commits <football365bot@gmail.com>
2025-10-05 21:39:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 from config_schema import get_settings
+import os
+PORT = int(os.getenv("PORT", 8080))
 
 settings = get_settings()
 
 BOT_TOKEN = settings.bot_token
 CHANNEL_ID = settings.channel_id
 WEBHOOK_HOST = settings.webhook_host
+WEBHOOK_PATH = f"/webhook{BOT_TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
