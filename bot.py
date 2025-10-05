ebfc7fe3ef6828ad528c922379c2bcb095d6e230
football365bot-commits <football365bot@gmail.com>
2025-10-05 20:21:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 import os
-PORT = int(os.getenv("PORT", 8443))
+PORT = int(os.getenv("PORT", 10000))
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
