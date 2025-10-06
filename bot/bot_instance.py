c722ae51bc7d3ba641377ca34c440bc6e3aeec22
football365bot-commits <football365bot@gmail.com>
2025-10-06 09:37:17 +0300

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import DefaultBotProperties
+from aiogram.client.bot import DefaultBotProperties
 
 BOT_TOKEN = "BOT_TOKEN"
 
@@ -7,6 +7,8 @@ bot = Bot(
     token=BOT_TOKEN,
     default=DefaultBotProperties(parse_mode="HTML")
 )
+    disable_web_page_preview=True,
+    protect_content=True
 dp = Dispatcher(bot)
 
 from bot.routers.start import router as start_router  # noqa
