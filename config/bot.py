5a7fcb56bf635b0169db2231b4404deeb80f7b81
football365bot-commits <football365bot@gmail.com>
2025-10-06 13:45:24 +0300

    Create bot.py

---
+++ config/bot.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Загружаем .env файл
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
+CHANNEL_ID = os.getenv("CHANNEL_ID")
