215cf7b9454ed187a2aa78046905abc3ab17ac00
football365bot-commits <football365bot@gmail.com>
2025-10-06 16:50:11 +0300

    Update setting.py

--- config/setting.py
+++ config/setting.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем .env файл
-load_dotenv()
+load_dotenv()  # загружаем переменные из .env
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") 
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+WEBHOOK_HOST = os.getenv("WEBHOOK_URL")  # https://bot-1-juge.onrender.com
+WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"   # путь на сервере
+WEBHOOK_URL = WEBHOOK_HOST.rstrip("/") + WEBHOOK_PATH  # полный URL
