7512b1b4efdb853b3a4b9c1d5738117a74b35c53
football365bot-commits <football365bot@gmail.com>
2025-10-07 14:54:48 +0300

    Create create_bot.py

---
+++ create_bot.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from decouple import config
+
+BOT_TOKEN = config("BOT_TOKEN")
+ADMIN_ID = config("ADMIN_ID")
+HOST = config("HOST")
+PORT = int(config("PORT"))
+WEBHOOK_PATH = f'/{BOT_TOKEN}'
+BASE_URL = config("BASE_URL")
+CHANNEL_USERNAME = "@your_channel"  # Публичный канал
+
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
