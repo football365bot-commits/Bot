1c7a67765bb31d79c2785ac6ec18734931300627
football365bot-commits <football365bot@gmail.com>
2025-10-08 17:22:59 +0300

    Update sub_check_message.py

--- start/sub_check_message.py
+++ start/sub_check_message.py
@@ -1,1 +1,16 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message 
+from create_bot import bot, CHANNEL_ID
 
+class IsSubskcribedMessage(BaseFilter):
+    async def __call__(self, message: Message):
+        user_id = message.from_user.id
+        sub = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id
+        if sub.status != "left":
+            return True 
+        else:
+            await bot.send_message(
+                chat_id=user_id,
+                text="Подпишитесь чтобы продолжить!",
+                reply_markup=
+            )
