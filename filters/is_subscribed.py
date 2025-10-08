b295ea80e6425eb116dd4838d5adfcd9b6df7ab8
football365bot-commits <football365bot@gmail.com>
2025-10-08 15:48:01 +0300

    Update is_subscribed.py

--- filters/is_subscribed.py
+++ filters/is_subscribed.py
@@ -2,7 +2,7 @@ from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from create_bot import bot, CHANNEL_ID
 
-class IsSubscribed(BaseFilter):
+class IsSubscribedMessage(BaseFilter):
     async def __call__(self, message:Message):
         user_id = message.from_user.id
         sub = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
