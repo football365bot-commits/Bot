0ecf6fc7a0198cfc8298e4c3fecaad11b2b9c692
football365bot-commits <football365bot@gmail.com>
2025-10-08 19:21:56 +0300

    Update sub_handlers_start.py

--- start/sub_handlers_start.py
+++ start/sub_handlers_start.py
@@ -2,12 +2,14 @@ from aiogram.filters import CommandStart
 from aiogram import Router
 from aiogram.types import Message
 from create_bot import bot
+from start.sub_link import sub_link_buttons  # ← твоя клавиатура
 
 router = Router()
-
-
 
 @router.message(CommandStart())
 async def start_command(message: Message):
     await message.answer(
-        f"Добро пожаловать, {message.from_user.full_name}! 👋\n")
+        f"Добро пожаловать, {message.from_user.full_name}! 👋\n"
+        "Проверьте подписку, чтобы продолжить 👇",
+        reply_markup=sub_link_buttons  # ← используем твою клавиатуру
+    )
