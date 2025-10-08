5c5100570b1bcb77a91491cbfbb09c7c11988c15
football365bot-commits <football365bot@gmail.com>
2025-10-08 19:19:39 +0300

    Update sub_done.py

--- start/sub_done.py
+++ start/sub_done.py
@@ -2,6 +2,6 @@ from aiogram import types, Router, F
 
 router = Router()
 
-@router.callback_query(F.data == " sub_done")
+@router.callback_query(F.data == "sub_done")
 async def sub_done(call: types.CallbackQuery):
-    await call.message.edit_text("Вы подписались🤝")
+    await call.message.edit_text("Вы подписались 🤝")
