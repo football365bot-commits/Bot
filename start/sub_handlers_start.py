6d9738c4c29a02bac34bbd7353fc582486de0a7f
football365bot-commits <football365bot@gmail.com>
2025-10-08 19:08:12 +0300

    Update sub_handlers_start.py

--- start/sub_handlers_start.py
+++ start/sub_handlers_start.py
@@ -1,1 +1,13 @@
+from aiogram.filters import CommandStart
+from aiogram import Router
+from aiogram.types import Message
+from create_bot import bot
 
+router = Router()
+
+
+
+@router.message(CommandStart())
+async def start_command(message: Message):
+    await message.answer(
+        f"Добро пожаловать, {message.from_user.full_name}! 👋\n")
