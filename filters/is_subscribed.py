8e888294a985c05be4fda56c7b4b78c74e65db08
football365bot-commits <football365bot@gmail.com>
2025-10-08 15:03:51 +0300

    Update is_subscribed.py

--- filters/is_subscribed.py
+++ filters/is_subscribed.py
@@ -1,12 +1,17 @@
 from aiogram.filters import BaseFilter
-from aiogram.types import CallbackQuery
+from aiogram.types import Message
 from create_bot import bot, CHANNEL_ID
 
 class IsSubscribed(BaseFilter):
-    async def __call__(self, call: CallbackQuery) -> bool:
-        try:
-            member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=call.from_user.id)
-            return member.status in ["member", "administrator", "creator"]
-        except Exception as e:
-            print(f"Ошибка проверки подписки: {e}")
-            return False
+    async def __call__(self, message:Message):
+        user_id = message.from_user.id
+        sub = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
+        if sub.status != "left":
+            return True
+        else:
+            await bot.send_message(
+                chat_id=user_id,
+                text="Подпишитесь,чтобы получить доступ!"
+                repla
+            )
+            
