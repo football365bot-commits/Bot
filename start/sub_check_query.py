314b6c0fb1eedfd17b272e2ea6caa193a7d5615b
football365bot-commits <football365bot@gmail.com>
2025-10-09 09:09:23 +0300

    Update sub_check_query.py

--- start/sub_check_query.py
+++ start/sub_check_query.py
@@ -1,34 +1,13 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery
 from create_bot import bot, CHANNEL_ID
-from start.sub_link import sub_link_buttons
-from language.lang_keyboard import language_keyboard
+
 
 class IsSubscribedQuery(BaseFilter):
     async def __call__(self, call: CallbackQuery) -> bool:
-        user_id = call.from_user.id
         try:
-            # Проверяем, подписан ли пользователь
-            sub = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
-
-            if sub.status != "left":
-                await call.message.edit_text(
-                    "Спасибо за подписку!✅\nВыберите язык:",
-                    reply_markup=language_keyboard
-                )
-                return True
-            else:
-                await bot.send_message(
-                    chat_id=user_id,
-                    text="❌ Подпишитесь, чтобы продолжить!",
-                    reply_markup=sub_link_buttons
-                )
-                return False
-
+            sub = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=call.from_user.id)
+            return sub.status != "left"
         except Exception as e:
             print(f"Ошибка проверки подписки: {e}")
-            await bot.send_message(
-                chat_id=user_id,
-                text="⚠️ Не удалось проверить подписку. Попробуйте позже."
-            )
             return False
