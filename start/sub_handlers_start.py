cddcf4db7b4444bba3433e3860ed3a0150563253
football365bot-commits <football365bot@gmail.com>
2025-10-09 16:54:40 +0300

    Update sub_handlers_start.py

--- start/sub_handlers_start.py
+++ start/sub_handlers_start.py
@@ -11,5 +11,16 @@ async def start_command(message: Message):
     await message.answer(
         f"Добро пожаловать, {message.from_user.full_name}! 👋\n"
         "Проверьте подписку, чтобы продолжить 👇",
+        
+        member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=call.from_user.id)
+
+    if member.status != "left":
+        await call.message.edit_text(
+            "Спасибо за подписку!✅\nВыберите язык:",
+            reply_markup=await kb.language_keyboard()
+        )
+    else:
+        await call.message.edit_text(
+            "❌ Подпишитесь на канал, чтобы продолжить!",
         reply_markup=sub_link_buttons  # ← используем твою клавиатуру
     )
