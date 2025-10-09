3bfda2f9b75f22484e75351999c3ba263e5c0334
football365bot-commits <football365bot@gmail.com>
2025-10-09 12:52:24 +0300

    Update sub_done.py

--- start/sub_done.py
+++ start/sub_done.py
@@ -2,11 +2,10 @@ from aiogram import Router, types, F
 from language.lang_keyboard import language_keyboard
 from start.sub_link import sub_link_buttons
 from create_bot import bot, CHANNEL_ID
-from start.sub_check_query import 
-IsSubscribedQuery
+
 router = Router()
 
-@router.callback_query(F.data == "sub_done", IsSubscribedQuery)
+@router.callback_query(F.data == "sub_done")
 async def sub_done(call: types.CallbackQuery):
     member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=call.from_user.id)
 
