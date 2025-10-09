f22a169e4b939e7788af87ca2cc61edbb6ff25ee
football365bot-commits <football365bot@gmail.com>
2025-10-09 10:13:12 +0300

    Update sub_done.py

--- start/sub_done.py
+++ start/sub_done.py
@@ -1,25 +1,14 @@
 from aiogram.types import CallbackQuery
-from aiogram import types, Router, F
+from aiogram import Router, F
 from language.lang_keyboard import language_keyboard
-from create_bot import bot
-from create_bot import CHANNEL_ID
 from start.sub_check_query import IsSubscribedQuery
-
-
 
 router = Router()
 
 @router.callback_query(F.data == "sub_done", IsSubscribedQuery())
-async def sub_done(call: types.CallbackQuery):
-    
+async def sub_done(call: CallbackQuery):
+    # Подписка уже проверена фильтром
     await call.message.edit_text(
-                "Спасибо за подписку!✅\nВыберите язык:",
-                reply_markup=language_keyboard
-            )
-        else:
-            await bot.send_message(
-                chat_id=call.from_user.id,
-                text="❌ Подпишитесь, чтобы продолжить!"
-            )
-    except Exception as e:
-        print(f"Ошибка при проверке подписки в хендлере: {e}")
+        "Спасибо за подписку!✅\nВыберите язык:",
+        reply_markup=language_keyboard
+    )
