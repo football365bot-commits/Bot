3d6f0822848ddd1e9b9e6bf0dc0f9a23351009ee
football365bot-commits <football365bot@gmail.com>
2025-10-09 19:12:09 +0300

    Update sub_done.py

--- start/sub_done.py
+++ start/sub_done.py
@@ -1,12 +1,12 @@
 from aiogram import Router, types
-from aiogram.filters import Text
+from aiogram.filters import F
 from create_bot import bot, CHANNEL_ID
 from start.sub_link import sub_link_buttons
 from language.lang_keyboard import language_keyboard
 
 router = Router()
 
-@router.callback_query(Text("sub_done"))
+@router.callback_query(F.data == "sub_done"))
 async def sub_done(call: types.CallbackQuery):
     """
     Обработчик нажатия кнопки "Проверить подписку"
