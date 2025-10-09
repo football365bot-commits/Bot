db35e14d2a68b2639ab354a3b2ce14e8de81371c
football365bot-commits <football365bot@gmail.com>
2025-10-09 10:05:03 +0300

    Update sub_message_hen.py

--- start/sub_message_hen.py
+++ start/sub_message_hen.py
@@ -1,6 +1,9 @@
 from aiogram import Router, types
 from start.sub_link import sub_link_buttons
 from aiogram.types import Message 
+from start.sub_check_message import IsSubscribedMessage
+
+
 router = Router()
 
 @router.message(IsSubscribedMessage())
