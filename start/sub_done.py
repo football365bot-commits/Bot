da9c297688d102d513463a7b4315b9746b53fee5
football365bot-commits <football365bot@gmail.com>
2025-10-09 11:04:15 +0300

    Update sub_done.py

--- start/sub_done.py
+++ start/sub_done.py
@@ -1,23 +1,23 @@
 from aiogram import Router, types, F
 from language.lang_keyboard import language_keyboard
-from start.sub_check_query import IsSubscribedQuery
 from start.sub_link import sub_link_buttons  # кнопки для подписки
 from create_bot import bot, CHANNEL_ID
 
 router = Router()
 
-@router.callback_query(F.data == "sub_done", IsSubscribedQuery())
+@router.callback_query(F.data == "sub_done")
 async def sub_done(call: types.CallbackQuery):
-    # Пользователь подписан — показываем выбор языка
-    await call.message.edit_text(
-        "Спасибо за подписку!✅\nВыберите язык:",
-        reply_markup=language_keyboard
-    )
+    member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=call.from_user.id)
     
-    
-@router.callback_query(F.data == "sub_done", IsSubscribedQuery())
-async def sub_not_subscribed(call: types.CallbackQuery):
-    await call.message.answer(
+    if member.status != "left":
+        # Пользователь подписан — показываем выбор языка
+        await call.message.edit_text(
+            "Спасибо за подписку!✅\nВыберите язык:",
+            reply_markup=language_keyboard
+        )
+    else:
+        # Пользователь не подписан — показываем сообщение с кнопкой подписки
+        await call.message.answer(
             "❌ Подпишитесь на канал, чтобы продолжить!",
             reply_markup=sub_link_buttons
         )
