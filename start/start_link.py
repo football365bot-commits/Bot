fdaa3960780b35346ca1d5e46ecb0293768e61bd
football365bot-commits <football365bot@gmail.com>
2025-10-10 11:31:15 +0300

    Update start_link.py

--- start/start_link.py
+++ start/start_link.py
@@ -1,1 +1,14 @@
-.
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from create_bot import CHANNEL_LINK  # ссылка на канал
+
+# Создаём inline-клавиатуру
+sub_link_buttons = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Подписаться✅", url=CHANNEL_LINK)  # кнопка с ссылкой на канал
+        ],
+        [
+            InlineKeyboardButton(text="Проверить подписку🔄", callback_data="")  # кнопка для проверки подписки
+        ]
+    ]
+)
