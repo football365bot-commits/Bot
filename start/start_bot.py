3f358999c6ea41616b4e915285fb99a753a326d5
football365bot-commits <football365bot@gmail.com>
2025-10-10 11:13:37 +0300

    Update start_bot.py

--- start/start_bot.py
+++ start/start_bot.py
@@ -1,1 +1,8 @@
-.
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+
+router = Router()
+
+@router.message(CommandStart())
+async def start_command(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.full_name}! 👋 Добро пожаловать!")
